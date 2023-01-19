60c1d7b33033ae9f65dd948e647e470d60b5c7ae
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-01-19 14:28:54 -0500

    Add script.js and change views when clicking submit

---
+++ script.js
@@ -0,0 +1,11 @@
+const submitBtn = document.querySelector('.card__cta');
+const firstView = document.querySelector('.card');
+const secondView = document.querySelector('.thankyou');
+
+const rates = document.querySelectorAll('.card__buttons');
+const rating = document.getElementById('rating');
+
+submitBtn.addEventListener('click', () => {
+  secondView.classList.toggle('inactive');
+  firstView.style.display = 'none';
+});
